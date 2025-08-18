30977334e03f43d2e9a99bb2b8ce984f168e2b1e
bijoyahmed02 <bijoyahmed818@gmail.com>
2025-08-18 23:54:21 +0600

    variable_constant

---
+++ task1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x=5, y=20;
+    int sum = x+y;
+    double z= 10.25;
+    char letter = 'A';
+    string text ="Hello";
+    bool Boolean=true;
+    const int day = 24;
+    const float pi = 3.14;
+
+    cout << "Hello world!" << endl;
+    cout << "Sum = " << sum<<endl;
+    cout << "Price is " << z<<endl;
+    cout << "Sum = " << x+y+z<<endl;
+    cout<< pi<<endl;
+    cout<< "Day "<<day;
+
+    return 0;
+}
