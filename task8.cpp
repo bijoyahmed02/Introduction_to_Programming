0ef0fd3b913c9505bd86dc3997b2a1ee16857358
Bijoy Ahamed <bijoyahmed818@gmail.com>
2025-08-31 00:33:31 +0600

    Type Casting Problem

---
+++ task8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a= 3.45; float b= 4.57;
+    float z = a + b;
+    int p = (int)a + (int) b;
+    cout<<"Z: "<<z<<endl;
+    cout<<"P: "<<p<<endl;
+    return 0;
+}
