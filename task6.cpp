7d089472e01086eb224d49ccf856a367be629b42
Bijoy Ahamed <bijoyahmed818@gmail.com>
2025-08-30 23:47:31 +0600

    String getline

---
+++ task6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string fullName;
+    cout << "Enter your full name: ";
+    getline(cin,fullName);
+    cout<<"Your Name is: "<<fullName;
+    return 0;
+}
