cb333ed729d89ba3b94ef5538f8dde84d02719ff
Bijoy Ahamed <bijoyahmed818@gmail.com>
2025-08-31 00:34:13 +0600

    Conditional problem

---
+++ task9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int hours = 20;
+    if (hours < 12) {
+    cout << "Good morning.";
+    } else if (hours < 18) {
+    cout << "Good Noon.";
+    } else {
+    cout << "Good evening.";
+
+    return 0;
+}
