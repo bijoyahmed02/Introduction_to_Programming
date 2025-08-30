fcaf800df9ac2b439a89e6302a14094db108ca24
bijoyahmed02 <bijoyahmed818@gmail.com>
2025-08-30 23:41:05 +0600

    User Input-output

---
+++ task2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    int sum;
+    cout << "Enter a number: ";
+    cin>>x;
+    cout<<"Enter another number: ";
+    cin>>y;
+    sum = x+y;
+    cout<<"The Sum is: "<<sum;
+    return 0;
+}
