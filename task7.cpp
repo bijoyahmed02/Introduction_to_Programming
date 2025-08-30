eee6d53a0ef823b50c7dde15e475343d1097906c
Bijoy Ahamed <bijoyahmed818@gmail.com>
2025-08-30 23:54:43 +0600

    using max, min, sqrt, round, log

---
+++ task7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    cout <<max(15,10)<< endl;
+    cout <<min(5,20)<< endl;
+    cout <<sqrt(4)<< endl;
+    cout <<round(2.6)<< endl;
+    cout <<log(1)<< endl;
+
+    return 0;
+}
